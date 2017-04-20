d5ea07c46169f56ebe220de6e66fbc087be973b8
Steve Chall <stevec@renci.org>
2017-04-20 17:48:07 -0400

    cleaner code_list construction that skips headers

--- create_code_matrices.py
+++ create_code_matrices.py
@@ -14,52 +14,41 @@
 
 import sys
 import os
-import json
-import re
-import ntpath
+import csv
 
 def read_cblm(path):
     print 'read_cblm("' + path + '")'
 
-def read_all_cblms(cblm_dir):
-    """  Get a list of all the rows in all the CBLMs. """
+def create_code_list(cblm_dir):
+    """  Read in all the CBLM files in cblm_dir, and return a list of all the
+         codes.
+    """
 
-    print 'read_all_cblms("' + cblm_dir + '")'
+    print 'create_code_list("' + cblm_dir + '")'
     cblm_files = []
     cblm_files += [fn for fn in os.listdir(cblm_dir) if fn.endswith("-CBLM.csv")]
-    #print(str(cblm_files), sep = '\n')
-    #print "\n".join(cblm_files)
     clist = []
     for i, cblm in enumerate(cblm_files):
         cblm_file = cblm_dir + '/' + cblm
-        with open(cblm_file) as cblm: # Read CBLM file as list, each item a row:
-             rows = cblm.read().split("\n");
-
-        for row in rows: # clist becomes a list of sublists, each sublist a row
-            r = row.split("\t") # Split each row into a list of items
-            clist.append(r)
-        clist.pop() # get rid of empty last row
+        with open(cblm_file, "rb") as cblm: 
+            reader = csv.reader(cblm, delimiter = '\t')
+            next(reader, None)
+            for row in reader: 
+                clist.append(row[4])
     return clist
-
-def create_unique_code_list(cblm_list):
-    """ Accepts cblm_list, a list of all the rows in all the CBLMs, and returns
-            a list of all the codes from cblm_list.
-    """
-    code_list = []
-    for row in cblm_list:
-        code_list.append(row[4])
-    return code_list
 
 def write_code_matrices(code_matrix_dir):
     print 'write_code_matrices("' + code_matrix_dir + '")'
-
 
 # main:
 cblm_dir = sys.argv[1]
 code_matrix_dir = sys.argv[2]
 
-unique_code_list = sorted(set(create_unique_code_list(read_all_cblms(cblm_dir))))
-print "\n".join(unique_code_list)
+unique_code_list = sorted(set(create_code_list(cblm_dir)))
+#unique_code_list = create_code_list(cblm_dir)
+#print "\n".join(unique_code_list)
+for i, code in enumerate(unique_code_list):
+    print str(i) + ": \"" + code + "\""
 write_code_matrices(code_matrix_dir)
     
 
