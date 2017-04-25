5a42a176adf5022fcd2bfc2b7c3e6e022493cadc
Steve Chall <stevec@renci.org>
2017-04-25 14:00:36 -0400

    build code matrix output file paths

--- create_code_matrices.py
+++ create_code_matrices.py
@@ -21,26 +21,46 @@ import pandas as pd
 def read_cblm(path):
     print 'read_cblm("' + path + '")'
 
-def create_code_list(cblm_dir):
+def create_code_list(cblm_files):
     """  Read in all the CBLM files in cblm_dir, and return a list of all the
          codes.
     """
-
-    print 'create_code_list("' + cblm_dir + '")'
-    cblm_files = []
-    cblm_files += [fn for fn in os.listdir(cblm_dir) if fn.endswith('-CBLM.csv')]
     clist = []
     for i, cblm in enumerate(cblm_files):
-        cblm_file = cblm_dir + '/' + cblm
-        with open(cblm_file, 'rb') as cblm: 
-            reader = csv.reader(cblm, delimiter = '\t')
-            next(reader, None)
+        with open(cblm, 'rb') as f: 
+            reader = csv.reader(f, delimiter = '\t')
+            next(reader, None) # Skip headers
             for row in reader: 
                 clist.append(row[4])
     return clist
 
-def write_code_matrices(code_matrix_dir):
-    print 'write_code_matrices('" + code_matrix_dir + "')'
+def get_cblm_file_list(cblm_dir):
+    cblm_files = []
+    cblm_files += [fn for fn in os.listdir(cblm_dir) if fn.endswith('-CBLM.csv')]
+    return cblm_files
+
+def get_cblm_path_list(cblm_dir, cblm_file_list):
+    return [cblm_dir + "/" + filename for filename in cblm_file_list]
+
+def build_cm_paths(cblm_files, cm_dir):
+    cm_files = []
+    for cblm_fname in cblm_files:
+        if cblm_fname.endswith("-CBLM.csv"):
+            cm_path = cm_dir + "/" + cblm_fname[:-7] + "M.csv"
+            cm_files.append(cm_path)
+        else:
+            print "build_cm_file_list: invalid CBLM file name" + cblm_fname
+    return cm_files
+
+def write_code_matrices(cblm_paths, code_matrix_dir):
+    print 'write_code_matrices(..., "' + code_matrix_dir + '")'
+    for i, cblm in enumerate(cblm_paths):
+        with open(cblm, 'rb') as f:
+            reader = csv.reader(f, delimiter = '\t')
+            next(reader, None) # Skip headers
+            #for row in reader:
+                #print row
+                
 
 def append_legend(file_path, code_list):
   with open(file_path, 'a') as f:
@@ -61,14 +81,25 @@ def initialize_data_frame(code_list):
 cblm_dir = sys.argv[1]
 code_matrix_dir = sys.argv[2]
 
-unique_code_list = sorted(set(create_code_list(cblm_dir)))
+cblm_files = get_cblm_file_list(cblm_dir)
+cblm_paths = get_cblm_path_list(cblm_dir, cblm_files)
+print "*** cblm paths: "
+print '\n'.join(cblm_paths)
+unique_code_list = sorted(set(create_code_list(cblm_paths)))
 #print '\n'.join(unique_code_list)
+cm_paths = build_cm_paths(cblm_files, code_matrix_dir)
+print "*** cm_paths: "
+print '\n'.join(cm_paths)
+"""
 for i, code in enumerate(unique_code_list):
     print str(i) + ': \"' + code + '\"'
+"""
 initialized_data_frame = initialize_data_frame(unique_code_list)
 #print(np.matrix(initialized_data_frame))
+"""
 with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
      print(initialized_data_frame)
+"""
 
 file_path = code_matrix_dir + '/empty.csv'
 initialized_data_frame.to_csv(path_or_buf = file_path, sep = '\t')
@@ -79,6 +110,6 @@ for row in initialized_data_frame:
     for val in row:
         print '{:2}'.format(val),
     print
-write_code_matrices(code_matrix_dir)
-    
 """
+write_code_matrices(cblm_paths, code_matrix_dir)
+    
