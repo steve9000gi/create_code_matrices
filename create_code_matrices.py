cca597c79c305a1730b9ed3da6f969b2eb022279
Steve Chall <stevec@renci.org>
2017-04-26 14:35:06 -0400

    clean up

--- create_code_matrices.py
+++ create_code_matrices.py
@@ -29,7 +29,7 @@ def create_code_list(cblm_files):
     for i, cblm in enumerate(cblm_files):
         with open(cblm, 'rb') as f: 
             reader = csv.reader(f, delimiter = '\t')
-            next(reader, None) # Skip headers
+            next(reader, None) # skip headers
             for row in reader: 
                 clist.append(row[4])
     return clist
@@ -41,13 +41,6 @@ def get_cblm_file_list(cblm_dir):
 
 def get_cblm_path_list(cblm_dir, cblm_file_list):
     return [cblm_dir + "/" + filename for filename in cblm_file_list]
-
-def build_cm_path(cblm_fname, cm_dir):
-    if cblm_fname.endswith("-CBLM.csv"):
-        return cm_dir + "/" + cblm_fname[:-7] + "M.csv"
-    else:
-        print "build_cm_file_list: invalid CBLM file name" + cblm_fname
-        return "xxx-CM.csv"
 
 def build_cm_paths(cblm_files, cm_dir):
     cm_files = []
@@ -70,27 +63,22 @@ def write_code_matrix(file_path, df, code_list):
 def populate_df(cblm, df, master_code_list):
     """ For each row in the cblm, get the code, find out which nodes the current
         node is linked to, find out the codes for each of those, and add 1 to the
-        values in two locations in the df that corresponds to those two codes.
+        values in the two locations (we're treating edges as non- or bi-
+        directional) in the data frame that corresponds to those two codes.
     """
-    print "populate_df(" + cblm + ",...)"
     popd_df = df.copy()
     cblm_df = pd.read_csv(cblm, sep='\t')
     curr_code_list = cblm_df["Code"].values.tolist()
-    #print "type(curr_code_list): " + type(curr_code_list).__name__
-    #print "type(master_code_list): " + type(master_code_list).__name__
     blm = cblm_df.loc[:,'0':]
     for i, row in blm.iterrows():
-        rlist = row.values.tolist() # want int indices, not nodeID col labels
+        rlist = row.values.tolist() # sequential indices, not nodeID col labels
         for j, elt in enumerate(rlist):
             if rlist[j] != 0:
                 x = master_code_list.index(curr_code_list[j])
                 y = master_code_list.index(curr_code_list[i])
                 popd_df.iloc[x, y] += 1
-                #popd_df.iloc[y, x] += 1
-                print "blm[" +  str(i) + ", " + str(j) + "]: <" + str(rlist[j]) + ">; type: " + type(rlist[j]).__name__ 
-
-        #print str(i) + ". type(row): " + type(row).__name__
-    #print "type(blm): " + type(blm).__name__
+                print "blm[" +  str(i) + ", " + str(j) + "]: <" + str(rlist[j]) \
+                    + ">; type: " + type(rlist[j]).__name__ 
     return popd_df
 
 def write_code_matrices(cblm_paths, cm_paths, df, code_list):
@@ -99,11 +87,10 @@ def write_code_matrices(cblm_paths, cm_paths, df, code_list):
         write_code_matrix(cm_paths[i], popd_df, code_list)        
 
 def initialize_data_frame(code_list):
-    """ Construct 2D square matrix with dimensions = length of code_list
+    """ Construct 2D square data frame with dimensions = length of code_list
     """
     dim = len(code_list)
     temp = [[0 for x in range(dim)] for y in range(dim)] 
-
     labels = map(str, range(0, dim))
     return pd.DataFrame(temp, columns=labels, index=labels, dtype=np.int8)
 
@@ -118,9 +105,6 @@ print '\n'.join(cblm_paths)
 unique_code_list = sorted(set(create_code_list(cblm_paths)))
 #print '\n'.join(unique_code_list)
 cm_paths = build_cm_paths(cblm_files, code_matrix_dir)
-print "*** " + str(len(cm_paths)) + " cm_paths: "
-print '\n'.join(cm_paths)
 df_template = initialize_data_frame(unique_code_list)
-file_path = code_matrix_dir + '/empty.csv'
 write_code_matrices(cblm_paths, cm_paths, df_template, unique_code_list)
     
